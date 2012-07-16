d21d249376ebbe2af4c600036c59b39674bbac3f
lincoln.liu <xianlinbox@gmail.com>
2012-07-17 00:56:16 +0800

    add User Micropost Model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
+  validates :content, :length=>{:maximum => 20}
+  validates :user_id, :length=>{:minimum => 1}
+  
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
+  match 'user/create' => 'users#new',:via => :get
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
